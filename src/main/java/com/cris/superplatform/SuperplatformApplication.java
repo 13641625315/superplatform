dc38c41ea7dc00fff38b838287404265d83b75dc
Cris <zwb_4411@qq.com>
2019-10-31 22:44:51 +0800

    Initial commit

---
+++ src/main/java/com/cris/superplatform/SuperplatformApplication.java
@@ -0,0 +1,13 @@
+package com.cris.superplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperplatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperplatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cris/superplatform/SuperplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cris.superplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperplatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
