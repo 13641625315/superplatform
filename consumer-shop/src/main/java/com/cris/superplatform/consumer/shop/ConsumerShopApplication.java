41583dbe5e9cbca2f6d6a77ea49749232dd6040b
Cris W Zhu <cris.w.zhu@pwc.com>
2019-11-05 16:13:38 +0800

    add consumer

---
+++ consumer-shop/src/main/java/com/cris/superplatform/consumer/shop/ConsumerShopApplication.java
@@ -0,0 +1,11 @@
+package com.cris.superplatform.consumer.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerShopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerShopApplication.class, args);
+    }
+}
--- consumer-shop/src/main/java/com/cris/superplatform/consumer/shop/pojo/User.java
+++ consumer-shop/src/main/java/com/cris/superplatform/consumer/shop/pojo/User.java
@@ -1,24 +1,12 @@
-package com.cris.superplatform.provider.user.entity;
+package com.cris.superplatform.consumer.shop.pojo;
 
-import javax.persistence.*;
 import java.math.BigDecimal;
 
-@Entity
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
-    @Column
     private String username;
-
-    @Column
     private String name;
-
-    @Column
     private Integer age;
-
-    @Column
     private BigDecimal balance;
 
     public Long getId() {
