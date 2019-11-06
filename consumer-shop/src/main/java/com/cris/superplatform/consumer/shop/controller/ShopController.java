8d0e14d1cd9370127a74961fed8f779c401a39ea
Cris W Zhu <cris.w.zhu@pwc.com>
2019-11-06 16:25:43 +0800

    add eurekaserver

--- consumer-shop/src/main/java/com/cris/superplatform/consumer/shop/controller/ShopController.java
+++ consumer-shop/src/main/java/com/cris/superplatform/consumer/shop/controller/ShopController.java
@@ -2,6 +2,7 @@ package com.cris.superplatform.consumer.shop.controller;
 
 import com.cris.superplatform.consumer.shop.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +12,11 @@ import org.springframework.web.client.RestTemplate;
 public class ShopController {
     @Autowired
     private RestTemplate restTemplate;
+    @Value("${user.userServiceUrl.findById}")
+    private String userServiceUrl;
 
     @GetMapping("/user/{id}")
     public User findById(@PathVariable Long id) {
-        return this.restTemplate.getForObject("http://localhost:8000/" + id, User.class);
+        return this.restTemplate.getForObject(userServiceUrl + id, User.class);
     }
 }
--- provider-user/src/main/java/com/cris/superplatform/provider/user/controller/UserController.java
+++ provider-user/src/main/java/com/cris/superplatform/provider/user/controller/UserController.java
@@ -5,9 +5,11 @@ import com.cris.superplatform.provider.user.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/provider-user")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
