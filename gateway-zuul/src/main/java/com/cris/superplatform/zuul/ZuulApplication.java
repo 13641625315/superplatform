cc006b94623dfa9e705947e2731de10095f8c086
Cris W Zhu <cris.w.zhu@pwc.com>
2020-01-08 17:44:17 +0800

    add config

--- gateway-zuul/src/main/java/com/cris/superplatform/zuul/ZuulApplication.java
+++ gateway-zuul/src/main/java/com/cris/superplatform/zuul/ZuulApplication.java
@@ -1,18 +1,30 @@
-package com.cris.superplatform.consumer.shop.feign.config;
+package com.cris.superplatform.zuul;
 
 import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 /**
- * ConsumerShopApplicationConfig
+ * ZuulApplication
  *
  * @author cris.zhu
- * @date 2019/11/28
+ * @date 2019/1/8
  */
-@Configuration
-public class ConsumerShopApplicationConfig {
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableHystrix
+@EnableCircuitBreaker
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+
     @Bean
     public ServletRegistrationBean getServlet() {
         HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
@@ -23,3 +35,4 @@ public class ConsumerShopApplicationConfig {
         return registrationBean;
     }
 }
+
--- my-consumer-experiment/src/main/java/com/cris/superplatform/consumer/my/experiment/ConsumerMyExperimentApplication.java
+++ my-consumer-experiment/src/main/java/com/cris/superplatform/consumer/my/experiment/ConsumerMyExperimentApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -18,10 +17,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @EnableHystrix
 @EnableCircuitBreaker
-@EnableHystrixDashboard
 public class ConsumerMyExperimentApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerMyExperimentApplication.class, args);
     }
+
 }
 
